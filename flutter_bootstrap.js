8054af92fdc63f97906a243a276fa7cecd05f067
Utsab404 <jerry.d.silentkiller@gmail.com>
2025-01-02 20:40:33 +0545

    deployment build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2330413488"
+    serviceWorkerVersion: "1440430197"
   }
 });
